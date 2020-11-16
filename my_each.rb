e2bb5752822ce60b30e1efdf34a2f132b0ab12fc
Aiden <aiden.marques94@gmail.com>
2020-11-16 16:05:45 -0500

    commit tree 11dd9bff4818792305436fdb5beb85aed60d4d4b

--- my_each.rb
+++ my_each.rb
@@ -3,11 +3,12 @@ def my_each(collection) # put argument(s) here
   while i < collection.length
     yield (collection[i])
     i += 1 
-  end 
+  end
+  collection.to_a 
 end
 
 collection = [1, 2, 3, 4]
 my_each(collection) do |i|
-  puts i
+  i
 end 
 
